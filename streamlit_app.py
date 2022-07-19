4b5acf76ca258f660ca57237ceec11a584ae822d
Dre Paje - Globe <109566893+janpajeglobe@users.noreply.github.com>
2022-07-19 10:47:55 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,11 @@
 
 import streamlit
 
-streamlit.title('Hello is the world')
-streamlit.text('Streamlits')
+streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+🥣 🥗 🐔 🥑🍞
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
